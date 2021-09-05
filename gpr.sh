26afe7fd0ef49f8fad48497a70d66468ad7e9df5
carseven <carles.serra@hotmail.es>
2021-09-06 00:52:06 +0200

    feat(aliases): Github PR open in browser

--- gpr.sh
+++ gpr.sh
@@ -1,14 +1,7 @@
-if [ -z $1] 
-    then
-        echo 'Failed to open a pull request.'
-    else
-        github_url=$(git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git:://)#http://#' -e 's@com:@com/@' -e 's%\.git$%%');
-        echo $github_url
-        branch_name=$(git symbolic-ref HEAD 2>/dev/null);
-        echo $branch_name
-        clean_remote_branch=${branch_name#refs/heads/*}
-        echo $clean_remote_branch
-        pr_url=$($github_url"/compare/"$clean_remote_branch"..."$1);
-        echo $pr_url
-        open $pr_url
-    fi
+github_url=$(git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git:://)#http://#' -e 's@com:@com/@' -e 's%\.git$%%');
+branch_name=$(git symbolic-ref HEAD 2>/dev/null);
+clean_remote_branch=${branch_name#refs/heads/*};
+pr_url="${github_url}/compare/${clean_remote_branch}...main";
+echo "Genarate PR: ${clean_remote_branch} -> main";
+echo "URL: ${pr_url}";
+open "${pr_url}";
