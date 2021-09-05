7fb3a587194129d0b30c7f0ef82e49c397748832
carseven <carles.serra@hotmail.es>
2021-09-06 00:18:30 +0200

    feat(zshrc): Add more git aliases

--- gpr.sh
+++ gpr.sh
@@ -1,9 +1,9 @@
-if [ $? -eq 0]; then
-        # origin  https://github.com/carseven/.dotfiles.git (fetch) -> https://github.com/carseven/.dotfiles.git
+if [ $? -eq 0]; 
+    then
         github_url=$(git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git:://)#http://#' -e 's@com:@com/@' -e 's%\.git$%%');
-        branch_name=`feature/$(git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3)`
-        pr_url=$($github_url"/comapare/master..."$branch_name)
-        echo '${pr_url}'
+        branch_name=$(git symbolic-ref HEAD 2>/dev/null);
+        clean_remote_branch=${branch_name#refs/heads/*}
+        pr_url=$($github_url"/compare/master..."$branch_name);
         open $pr_url
     else
         echo 'Failed to open a pull request.'
