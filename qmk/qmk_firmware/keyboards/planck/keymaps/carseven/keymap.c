e270431513af10554ed08ccf0e7eded12a6f4f0b
carseven <carles.serra@hotmail.es>
2021-09-12 01:32:40 +0200

    feat: Add tap/mod shift and caplocks. New cdot alias. Vscode decrease group size with hyper+u

--- qmk/qmk_firmware/keyboards/planck/keymaps/carseven/keymap.c
+++ qmk/qmk_firmware/keyboards/planck/keymaps/carseven/keymap.c
@@ -18,7 +18,8 @@ enum planck_keycodes {
 #define TABNUM LT(_NUM, KC_TAB) // Hold to move to layer NUM. Un tap es el tab.
 #define RAISE LT(_RAISE, KC_ENT) // Hold to move to layer Raise. Un tap es el enter.
 #define LOWER LT(_LOWER, KC_BSPC) // Hold to move to layer lower. Un tap es el backspace.
-#define HYP_ESC HYPR_T(KC_ESC)// Hold=Hyper and Tap=ESC
+#define HYP_ESC HYPR_T(KC_ESC) // Hold=Hyper and Tap=ESC
+#define SHIFT_CAPS LSFT_T(KC_CAPSLOCK) // Hold=Shift and tap=Toggle CAPLOCKS
  
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -30,14 +31,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Hyper| Ctrl | Alt  | GUI  |Lower |Space | Space|Raise | Left | Down |  Up  |Right |
+ * | Del  | Ctrl | Alt  | GUI  |Lower |Space | Space|Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
   TABNUM,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   HYP_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-  KC_HYPR,   KC_LCTL, KC_LALT, KC_LGUI, LOWER,   SPCMOV,  SPCMOV,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  SHIFT_CAPS,KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+  KC_DEL,    KC_LCTL, KC_LALT, KC_LGUI, LOWER,   SPCMOV,  SPCMOV,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
@@ -78,9 +79,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* MOV
 * ,-----------------------------------------------------------------------------------.
-* |      |      |      |      |      | PgUp | CIns |      |      |      | SIns | Del  |
+* |      |      |      |      |      | PgUp | CIns |      |      |      | SIns | Bksp |
 * |------+------+------+------+------+-------------+------+------+------+------+------|
-* | Bksp | Home | End  | Shift| Ctrl |PgDown| Left | Down |  Up  | Right|      | Caps |
+* |      | Home | End  | Shift| Ctrl |PgDown| Left | Down |  Up  | Right|      |      |
 * |------+------+------+------+------+------|------+------+------+------+------+------|
 * |      |      |      |      |      |      |      |      |      |      |      |      |
 * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -91,27 +92,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * - P outputs Shift + Insert
 */
 [_MOV] = LAYOUT_planck_grid(
-  XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_PGUP , C(KC_INS) , XXXXXXX , XXXXXXX , XXXXXXX  , S(KC_INS) , KC_DEL     ,
-  KC_BSPC , KC_HOME , KC_END  , KC_LSFT , KC_LCTL , KC_PGDN , KC_LEFT   , KC_DOWN , KC_UP   , KC_RIGHT , XXXXXXX   , KC_CAPSLOCK,
-  _______ , XXXXXXX , XXXXXXX , KC_CAPS , XXXXXXX , XXXXXXX , XXXXXXX   , XXXXXXX , XXXXXXX , XXXXXXX  , XXXXXXX   , _______    ,
+  XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_PGUP , C(KC_INS) , XXXXXXX , XXXXXXX , XXXXXXX  , S(KC_INS) , KC_BSPC ,
+  KC_BSPC , KC_HOME , KC_END  , KC_LSFT , KC_LCTL , KC_PGDN , KC_LEFT   , KC_DOWN , KC_UP   , KC_RIGHT , XXXXXXX   , _______,
+  _______ , XXXXXXX , XXXXXXX , KC_CAPS , XXXXXXX , XXXXXXX , XXXXXXX   , XXXXXXX , XXXXXXX , XXXXXXX  , XXXXXXX   , _______,
   _______ , _______ , _______ , _______ , XXXXXXX , _______ , _______   , KC_ENT  , _______ , _______  , _______   , _______
 ),
 
 /* NUM
 * ,-----------------------------------------------------------------------------------.
-* | **** |  F1  |  F2  |  F3  |  F4  |  F5  |      |   7  |   8  |   9  |   *  | Del  |
+* | **** |  F1  |  F2  |  F3  |  F4  |  F5  |      |   7  |   8  |   9  |   *  | Bksp |
 * |------+------+------+------+------+-------------+------+------+------+------+------|
-* |      |  F6  |  F7  |  F8  |  F9  |  F10 | Bksp |   4  |   5  |   6  |   +  |      |
+* |      |  F6  |  F7  |  F8  |  F9  |  F10 |  Del |   4  |   5  |   6  |   +  | Del  |
 * |------+------+------+------+------+------|------+------+------+------+------+------|
-* |      |      |      |      |  F11 |  F12 | Enter|   1  |   2  |   3  |   -  |      |
+* |      |      |      |      |  F11 |  F12 |      |   1  |   2  |   3  |   -  | Enter|
 * |------+------+------+------+------+------+------+------+------+------+------+------|
 * |      |      |      |      |      |      |      |   0  |   ,  |   .  |      |      |
 * `-----------------------------------------------------------------------------------'
 */
 [_NUM] = LAYOUT_planck_grid(
-  _______ , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , XXXXXXX , KC_7 , KC_8    , KC_9    , KC_PAST , KC_DEL  ,
-  XXXXXXX , KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_BSPC , KC_4 , KC_5    , KC_6    , KC_PPLS , XXXXXXX ,
-  XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_F11  , KC_F12  , KC_ENT  , KC_1 , KC_2    , KC_3    , KC_PMNS , XXXXXXX ,
+  _______ , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , XXXXXXX , KC_7 , KC_8    , KC_9    , KC_PAST , KC_BSPC,
+  XXXXXXX , KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_DEL ,  KC_4 , KC_5    , KC_6    , KC_PPLS , KC_DEL ,
+  XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_F11  , KC_F12  , XXXXXXX  ,KC_1 , KC_2    , KC_3    , KC_PMNS , KC_ENT ,
   XXXXXXX , XXXXXXX , XXXXXXX , _______ , XXXXXXX , XXXXXXX , XXXXXXX , KC_0 , KC_COMM , KC_DOT  , XXXXXXX , XXXXXXX
 ),
 
