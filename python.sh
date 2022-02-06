2b2c735d7ef272c8ccbbe1a1e9b220d710670459
carseven <carles.serra@hotmail.es>
2022-02-06 14:32:54 +0100

    Remove poetry from python.sh

--- python.sh
+++ python.sh
@@ -6,10 +6,3 @@ brew install openssl readline sqlite3 xz zlib
 curl https://pyenv.run | bash
 # Uninstall: rm -fr ~/.pyenv
 
-
-# Poetry
-# This temporarily sticky sets our Python version to the system Python, and installs poetry with this version.
-pyenv shell system
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-pyenv shell --unset
-# Uninstall: python get-poetry.py --uninstall or POETRY_UNINSTALL=1 python get-poetry.py
