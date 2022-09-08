09d41d8c5a21574a4dcdefba5eef601bb9406ead
carseven <carles.serra@hotmail.es>
2022-09-08 19:49:19 +0200

    fix(macos): deactivate safari development settings
    Find a performance bug on m1 when left click of safari, really slowns downs

--- setup/scripts/macos.sh
+++ setup/scripts/macos.sh
@@ -111,13 +111,13 @@ defaults write com.apple.Safari ShowFavoritesBar -bool false
 defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
 defaults write org.m0k.transmission IncompleteDownloadFolder -string "~/Downloads/Incomplete"
 
-# Enable the Develop menu and the Web Inspector in Safari
-defaults write com.apple.Safari IncludeDevelopMenu -bool false
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool false
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool false
+# Enable the Develop menu and the Web Inspector in Safari - Active(true)
+# defaults write com.apple.Safari IncludeDevelopMenu -bool true
+# defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+# defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 
 # Add a context menu item for showing the Web Inspector in web views
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+# defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
 ## ----------------------------------------
 ##  General
