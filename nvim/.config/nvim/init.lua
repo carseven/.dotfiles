c63549c30c8d595e4530064bb0ba5f037b08ff86
carseven <carles.serra@hotmail.es>
2023-01-29 18:57:00 +0100

    feat(nvim): set dracula color theme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -46,6 +46,7 @@ require('packer').startup(function(use)
   use 'lewis6991/gitsigns.nvim'
 
   use 'navarasu/onedark.nvim' -- Theme inspired by Atom
+  use 'Mofiqul/dracula.nvim' -- Dracula theme
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
@@ -117,7 +118,7 @@ vim.wo.signcolumn = 'yes'
 
 -- Set colorscheme
 vim.o.termguicolors = true
-vim.cmd [[colorscheme onedark]]
+vim.cmd [[colorscheme dracula]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
@@ -153,7 +154,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 require('lualine').setup {
   options = {
     icons_enabled = false,
-    theme = 'onedark',
+    theme = 'dracula',
     component_separators = '|',
     section_separators = '',
   },
