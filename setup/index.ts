adc70e54aac12333de67ae3e1a53fb2f2993f047
carseven <carles.serra@hotmail.es>
2023-08-16 20:54:39 +0200

    feat: add mas install

--- setup/index.ts
+++ setup/index.ts
@@ -4,12 +4,14 @@ import {
   BREW_FONTS,
   BREW_PACKAGES as BREW_MACOS_PACKAGES,
 } from "./src/config/brew.config.ts";
+import { MAS_PACKAGES } from "./src/config/mas.config.ts";
 import { STOW_DIRECTORIES } from "./src/config/stow.config.ts";
 import { VSCODE_EXTENSIONS } from "./src/config/vscode.config.ts";
 import { CliArguments } from "./src/services/cli-args.service.ts";
 import { Logger } from "./src/services/logger.service.ts";
 import { Task, TaskRunner } from "./src/services/task-runner.service.ts";
 import { installBrewPackages } from "./src/tasks/brew.ts";
+import { installMasPackages } from "./src/tasks/mas.ts";
 import { stowDirectories } from "./src/tasks/stow.ts";
 import { vscodeInstallExtensions } from "./src/tasks/vscode.ts";
 import { createDirectory } from "./src/utils/directory.utils.ts";
@@ -23,14 +25,14 @@ const os = await $`uname -s`.text();
 
 let tasks: Task[] = [];
 if (os === "Darwin") {
-  logger.info("Running script on MacOS...");
+  logger.info("[INFO] Running script on MacOS...");
   const packages = BREW_MACOS_PACKAGES;
   const caskPackages = BREW_CASK_PACKAGES;
 
   tasks = [
     {
       name: "Create dev directory",
-      action: () => createDirectory("dev"),
+      action: () => createDirectory("dev", undefined, logger),
     },
     {
       name: "Install brew packages",
@@ -38,18 +40,17 @@ if (os === "Darwin") {
         installBrewPackages(
           [...packages, ...caskPackages, ...BREW_FONTS],
           os,
-          logger,
+          logger
         ),
     },
     {
       name: "Config zsh shell",
-      action: () => configSystemShell("zsh"),
+      action: () => configSystemShell("zsh", logger),
     },
     {
       name: "Install mas packages",
       action: () => {
-        logger.debug("TODO: Implement mas configuration");
-        return Promise.resolve();
+        return installMasPackages(MAS_PACKAGES, logger);
       },
     },
     {
@@ -58,7 +59,7 @@ if (os === "Darwin") {
         cloneRepository(
           "https://github.com/carseven/.dotfiles.git",
           Deno.env.get("HOME") || ".",
-          ".dotfiles",
+          ".dotfiles"
         ),
     },
     {
