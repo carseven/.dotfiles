724a57a25257cfb3853754cfa1b5e846ac3a86a5
carseven <carles.serra.vendrell@hp.com>
2026-02-19 11:15:14 +0100

    remove aerospace and sketchybar

--- runs/sketchybar/plugins/keyboard_layout.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-keyboard_layout=$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources | egrep -w 'KeyboardLayout Name' |sed -E 's/^.+ = \"?([^\"]+)\"?;$/\1/')
-
-sketchybar --set "$NAME" label="$keyboard_layout"
